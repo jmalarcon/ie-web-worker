9076d52f7e746beae909a8e7861cc388e3518f1b
KillerKiwi2005 <KillerKiwi2005@a5a29426-70fc-11de-9a45-5127b2b2b1b8>
2009-07-15 22:09:59 +0000

    Small comment cleanup

--- worker.js
+++ worker.js
@@ -12,6 +12,7 @@ if(!Worker)
 		var __timer = null ;
 		var __text = null ;
 		var __fileContent = null ;
+		var onmessage ;
 	
 		self.onerror = null ;
 		self.onmessage = null ;
@@ -25,9 +26,6 @@ if(!Worker)
 			}
 			return false ;
 		} ;
-		//var resultReceiver;
-		//var errorReceiver;
-		var onmessage ;
 
 		// Method that starts the threading
 		self.postMessage = function( text )
@@ -36,19 +34,21 @@ if(!Worker)
 			__iterate() ;
 			return true ;
 		} ;
+
 		var __iterate = function()
 		{
-			// Create an Iframe and execute code in that scope?
+			// Execute on a timer so we dont block (well as good as we can get in a single thread)
 			__timer = setTimeout(__onIterate,1);
 			return true ;
 		} ;
+
 		var __onIterate = function()
 		{
 			try
 			{
 				if ( "function" == typeof onmessage )
 				{
-					onmessage({ "data": __text });
+					onmessage({ "data" : __text });
 				}
 				return true ;
 			}
@@ -56,7 +56,7 @@ if(!Worker)
 			{
 				if ( "function" == typeof self.onerror )
 				{
-					return self.onerror( { "data": ex.message } ) ;
+					return self.onerror( { "message" : ex.message } ) ;
 				}
 			}
 			return false ;
@@ -92,7 +92,7 @@ if(!Worker)
 			return xmlhttp;
 		}
 
-		var http		= getHTTPObject()
+		var http = getHTTPObject()
 		http.open("GET", scriptFile, false)
 		http.send(null);
 
@@ -110,19 +110,11 @@ if(!Worker)
 					break;
 				default:
 					__fileContent = strResponse ;
-					// this should be in the scope of the instance of the outer class
 					// IE functions will become delagates of the instance of Worker
 					eval( __fileContent ) ;
 					/*
 					at this point we now have:
-					a variable "results = []"
-					a delagate "resultReceiver(event)"
-					a delagate "errorReceiver(event)"
-					and
 					a delagate "onmessage(event)"
-
-					BUT none are executed as of yet and all are only in the scope of this function call __onIterate
-					so once we exit they will no longer exist.  
 					*/
 					break;
 			}
@@ -130,7 +122,7 @@ if(!Worker)
 
 		self.importScripts = function(src)
 		{
-			// hack time
+			// hack time, this will import the script but not wait for it to load...
 			var script = document.createElement("SCRIPT") ;
 			script.src = src ;
 			script.setAttribute( "type", "text/javascript" ) ;
